2909869891bfe9d6065aaf89a59898625626b399
Steve Heydweiller <sh@fastly.com>
2019-12-18 14:05:30 -0600

    allow importing files from `@fastly/ember-test-helpers`
    Co-authored-by: Geoffrey Schorkopf <gschorkopf@gmail.com>
    Co-authored-by: Luis Flores <lflores@fastly.com>
    Co-authored-by: Peter Banka <peter.banka@gmail.com>
    Co-authored-by: Jason Maier <jason.maier@fastly.com>
    Co-authored-by: Steve Heydweiller <sh@fastly.com>
    Co-authored-by: Gavin Love <glove@fastly.com>
    Co-authored-by: Jeneve Parrish <jparrish@fastly.com>

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const BroccoliDebug = require('broccoli-debug');
+const debugTree = BroccoliDebug.buildDebugCallback('ember-test-helpers');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  treeForAddonTestSupport(tree) {
+    let input = debugTree(tree, 'addon-test-support:input');
+    
+    let output = this.preprocessJs(input, '/', this.name, {
+      registry: this.registry,
+    });
+
+    return debugTree(output, 'addon-test-support:output');
+  }
 };
